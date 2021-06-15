b9f09b176d05fd73b9876f963957f5d99fb4614a
Sneha Sil <snehasil413@gmail.com>
2021-06-15 12:43:30 -0500

    created project

---
+++ blueberry pancakes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace blueberry_pancakes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
